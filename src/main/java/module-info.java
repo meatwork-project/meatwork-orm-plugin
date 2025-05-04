73b1530f269050fefcc7c32c0db39e5ef12e7262
dbarb <d.barbarin@hotmail.fr>
2025-05-04 17:45:40 +0200

    Update module

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.meatwork.orm.plugin {
+	exports com.meatwork.orm.plugin;
+	requires maven.plugin.api;
+	requires maven.plugin.annotations;
+	requires freemarker;
+	requires maven.core;
+	requires org.yaml.snakeyaml;
+}
---
+++ src/test/java/module-info.java
@@ -0,0 +1,5 @@
+module com.meatwork.orm.plugin.test {
+	requires com.meatwork.orm.plugin;
+	requires org.junit.jupiter.api;
+	requires maven.core;
+}
